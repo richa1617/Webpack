61d02e4724d79e16c3d53cd951f1471fd436023b
Richa <richamahajan1617@gmail.com>
2023-06-21 18:31:52 +0200

    Added html plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const add=require("./other")
 const addImg=require("./add-image")
+require('../style.css');
+
+
+
 console.log(add(5,7))
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   entry: "./src/index.js",
@@ -18,10 +20,17 @@ module.exports = {
         type: "asset/inline",
       },
       { test: /\.css$/, 
-       use: "css-loader"
-       },
+         use: ["style-loader", "css-loader"] },
     ],
   },
+
+  plugins :[
+    new HtmlWebpackPlugin ({
+      title :"Webpack",
+      filename :"index.html",
+      template :"./src/template.html"
+    })
+  ]
 };
 
 //For path we need to provide absolute path.The __dirname is a special variable in Node.js that represents the directory name of the current module. It provides the absolute path of the current file.The path.resolve() function will return the absolute path by joining the __dirname and "dist"
